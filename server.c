70b6338cfd661ba30abe0ee7dc1c6b6595b584a8
Cristina Manzanares Urrutia <crmanzan@car2s4.42barcelona.com>
2024-01-25 16:46:56 +0100

    i dunno

--- server.c
+++ server.c
@@ -6,51 +6,32 @@
 /*   By: crmanzan <crmanzan@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 10:51:19 by crmanzan          #+#    #+#             */
-/*   Updated: 2024/01/25 14:28:45 by crmanzan         ###   ########.fr       */
+/*   Updated: 2024/01/25 16:10:45 by crmanzan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <signal.h>
-//#include "../libft/libft.h"
+#include "libft/libft.h"
 #include <unistd.h>
 
-void NOSECOMOSELLAMA(int signal, siginfo_t *info, void *context){
-
+static void	action(int signal, siginfo_t *info, void *context)
+{
+	return (0);
 }
 
+int	main(void)
+{
+	struct sigaction	s_sigaction;
 
-
-
-
-
-int main (int argc, char *argv[]) {
-	
-	pid_t pid;
-	pid = getpid();
-	struct sigaction s_sigaction;
-	if( argc != 2 ){
-		return (0);
-		//tuputamadrteasdzdslfkjemklnef
-	}
+	ft_putstr_fd("Server PID: ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putchar_fd('\n', 1);
+	s_sigaction.sa_sigaction = action;
+	s_sigaction.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &s_sigaction, NULL);
 	sigaction(SIGUSR2, &s_sigaction, NULL);
-
-	s_sigaction.sa_sigaction = NOSECOMOSELLAMA;
-	//s_sigaction.isa_sighandler = nosecomosellama;
-
-
-
-
-
-	printf("PID: %i\n", pid);
-	while(1){
+	while (1)
 		pause();
-	}
-
-
-
-	return 0;
+	return (0);
 }
-
-
