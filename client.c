03dcea3cff18a935c984fbc6d455255c09103eb7
Cristina <crmanzan@gmail.com>
2024-01-31 19:42:05 +0100

    norminette arreglada

--- client.c
+++ client.c
@@ -6,41 +6,42 @@
 /*   By: crmanzan <crmanzan@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 12:00:11 by crmanzan          #+#    #+#             */
-/*   Updated: 2024/01/31 19:17:03 by crmanzan         ###   ########.fr       */
+/*   Updated: 2024/01/31 19:33:50 by crmanzan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
 //  LINE 24: mascara de bits lo que hace es que pone todo lo que nosta en la 
-//  posicion "bit" en 0
+//  posicion "bit" en 0. RECUERDA: sigusr1 es bit 1y el usr2 es el 0
 
-void    send(int pid, char *string)
+void	send(int pid, char *string)
 {
-    int i;
-    int bit;
+	int	i;
+	int	bit;
 
-    i = 0;
-    bit = 0;
-    while (string[i]){
-        bit = 0;
-        while(bit < 8)
-        {
-            if (string[i] & (1 << bit))
-                kill(pid, SIGUSR1); // 1
-            else
-                kill(pid, SIGUSR2); // 0
-            usleep(250);
-            bit++;
-        }
-        i++;
-    }
+	i = 0;
+	bit = 0;
+	while (string[i])
+	{
+		bit = 0;
+		while (bit < 8)
+		{
+			if (string[i] & (1 << bit))
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			usleep(250);
+			bit++;
+		}
+		i++;
+	}
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-    if(argc != 3)
-        return (write(2, "Usage: ./client <PID> <message>\n", 32), 1);
-    send(ft_atoi(argv[1]), argv[2]);
-    return (0);
+	if (argc != 3)
+		return (write(2, "Usage: ./client <PID> <message>\n", 32), 1);
+	send(ft_atoi(argv[1]), argv[2]);
+	return (0);
 }
--- minitalk.h
+++ minitalk.h
@@ -6,17 +6,15 @@
 /*   By: crmanzan <crmanzan@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/27 18:31:47 by crmanzan          #+#    #+#             */
-/*   Updated: 2024/01/27 18:34:56 by crmanzan         ###   ########.fr       */
+/*   Updated: 2024/01/31 19:34:34 by crmanzan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-#include "libft/libft.h"
-#include <signal.h>
-#include <unistd.h>
-
-
+# include "libft/libft.h"
+# include <signal.h>
+# include <unistd.h>
 
 #endif
